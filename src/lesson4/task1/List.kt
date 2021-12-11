827dd496a360e9e99c4f1e150fe93d34a65ae5e5
eboblao <vidyagin.leha@gmail.com>
2021-12-12 00:29:42 +0300

    комент подправлен

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -120,7 +120,7 @@ fun buildSumExample(list: List<Int>) = list.joinToString(separator = " + ", post
  * по формуле abs = sqrt(a1^2 + a2^2 + ... + aN^2).
  * Модуль пустого вектора считать равным 0.0.
  */
-fun abs(v: List<Double>): Double = sqrt((v.map{it*it}.sum() ))
+fun abs(v: List<Double>): Double = sqrt((v.map { it * it }.sum()))
 
 /**
  * Простая (2 балла)
@@ -150,7 +150,7 @@ fun center(list: MutableList<Double>): MutableList<Double> {
  * представленные в виде списков a и b. Скалярное произведение считать по формуле:
  * C = a1b1 + a2b2 + ... + aNbN. Произведение пустых векторов считать равным 0.
  */
-fun times(a: List<Int>, b: List<Int>): Int = (a zip b).map {(x,y)->x*y}.sum()
+fun times(a: List<Int>, b: List<Int>): Int = (a zip b).map { (x,y) -> x * y }.sum()
 
 /**
  * Средняя (3 балла)
