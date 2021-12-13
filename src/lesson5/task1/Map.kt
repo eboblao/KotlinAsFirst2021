dca4868461fc85058cf27b52749dd2e1900e813b
eboblao <vidyagin.leha@gmail.com>
2021-12-13 17:47:20 +0300

    сделал задание некоторые

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -166,7 +166,15 @@ fun mergePhoneBooks(mapA: Map<String, String>, mapB: Map<String, String>): Map<S
  *   averageStockPrice(listOf("MSFT" to 100.0, "MSFT" to 200.0, "NFLX" to 40.0))
  *     -> mapOf("MSFT" to 150.0, "NFLX" to 40.0)
  */
-fun averageStockPrice(stockPrices: List<Pair<String, Double>>): Map<String, Double> = TODO()
+fun averageStockPrice(stockPrices: List<Pair<String, Double>>): Map<String, Double> {
+    val map = mutableMapOf<String, List<Double>>()
+    for ((initial,price) in stockPrices)
+        if (initial in map) map[initial] = map[initial]!! + price
+        else map[initial] = listOf(price)
+    val res = mutableMapOf<String, Double>()
+    for ((key,value) in map) res[key] = value.sum() / value.size.toDouble()
+    return res
+}
 
 /**
  * Средняя (4 балла)
@@ -208,7 +216,14 @@ fun canBuildFrom(chars: List<Char>, word: String): Boolean = TODO()
  * Например:
  *   extractRepeats(listOf("a", "b", "a")) -> mapOf("a" to 2)
  */
-fun extractRepeats(list: List<String>): Map<String, Int> = TODO()
+fun extractRepeats(list: List<String>): Map<String, Int> {
+    val map = mutableMapOf<String, Int>()
+    for (k in list){
+        if (k in map) map[k] = map[k]!! + 1
+        else map[k] = 1
+    }
+    return map.filter { it.value > 1 }
+}
 
 /**
  * Средняя (3 балла)
@@ -277,7 +292,14 @@ fun propagateHandshakes(friends: Map<String, Set<String>>): Map<String, Set<Stri
  *   findSumOfTwo(listOf(1, 2, 3), 4) -> Pair(0, 2)
  *   findSumOfTwo(listOf(1, 2, 3), 6) -> Pair(-1, -1)
  */
-fun findSumOfTwo(list: List<Int>, number: Int): Pair<Int, Int> = TODO()
+fun findSumOfTwo(list: List<Int>, number: Int): Pair<Int, Int> {
+    val mapPop = mutableMapOf<Int, Int>()
+    for (k in list.indices){
+        if (list[k] in mapPop) return Pair(mapPop.getOrDefault(list[k], -1), k)
+        mapPop[number - list[k]] = k
+    }
+    return Pair(-1,-1)
+}
 
 /**
  * Очень сложная (8 баллов)
